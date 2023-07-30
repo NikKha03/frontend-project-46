8deed9ad4aeb629aa238f8499c2688dd4b31a732
Nikolai Khalimendik <kolyakhalimendik@gmail.com>
2023-07-30 15:27:56 +0300

    modified code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 import * as fs from 'node:fs';
+import _ from 'lodash';
 
 const genDiff = (filepath1, filepath2) => {
   const readFile1 = fs.readFileSync(filepath1, 'utf-8');
   const readFile2 = fs.readFileSync(filepath2, 'utf-8');
-  return [readFile1, readFile2];
-  // return JSON.stringify(readFile1);
+  const parseFile1 = JSON.parse(readFile1);
+  const parseFile2 = JSON.parse(readFile2);
+
+  const getKeysFile1 = Object.keys(parseFile1);
+  const getKeysFile2 = Object.keys(parseFile2);
+  const getAllKeys = _.union(getKeysFile1, getKeysFile2);
+  const sortAllKeys = _.sortBy(getAllKeys);
+
+  const result = sortAllKeys.reduce((acc, key) => {
+    if (Object.hasOwn(parseFile1, key) && !Object.hasOwn(parseFile2, key)) {
+      acc.push([`${'- '}${key}`, parseFile1[key]]);
+      return acc;
+    }
+    if (!Object.hasOwn(parseFile1, key) && Object.hasOwn(parseFile2, key)) {
+      acc.push([`${'+ '}${key}`, parseFile2[key]]);
+      return acc;
+    }
+    if (Object.hasOwn(parseFile1, key) && Object.hasOwn(parseFile2, key)) {
+      if (parseFile1[key] === parseFile2[key]) {
+        acc.push([`${'  '}${key}`, parseFile1[key]]);
+        return acc;
+      }
+      acc.push([`${'- '}${key}`, parseFile1[key]]);
+      acc.push([`${'+ '}${key}`, parseFile2[key]]);
+      return acc;
+    }
+    return acc;
+  }, []);
+
+  return JSON.stringify(Object.fromEntries(result), null, 2);
 };
 
-console.log(genDiff('./src/file1.json', './src/file2.json'));
-
+// console.log(genDiff('./src/file1.json', './src/file2.json'));
 export default genDiff;
