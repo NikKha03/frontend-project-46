c9fd6a79ac376deb9f595ff080e80351708a8686
Nikolai Khalimendik <kolyakhalimendik@gmail.com>
2023-08-02 12:48:20 +0300

    modified src

--- src/index.js
+++ src/index.js
@@ -1,39 +1,16 @@
 import * as fs from 'node:fs';
-import _ from 'lodash';
+import path from 'path';
+import makeTree from './makeTree.js';
 
-const genDiff = (filepath1, filepath2) => {
-  const readFile1 = fs.readFileSync(filepath1, 'utf-8');
-  const readFile2 = fs.readFileSync(filepath2, 'utf-8');
+const getPath = (nameFile) => path.resolve('fixtures', nameFile);
+
+const genDiff = (file1, file2) => {
+  const readFile1 = fs.readFileSync(getPath(file1), 'utf-8');
+  const readFile2 = fs.readFileSync(getPath(file2), 'utf-8');
   const parseFile1 = JSON.parse(readFile1);
   const parseFile2 = JSON.parse(readFile2);
 
-  const getKeysFile1 = Object.keys(parseFile1);
-  const getKeysFile2 = Object.keys(parseFile2);
-  const getAllKeys = _.union(getKeysFile1, getKeysFile2);
-  const sortAllKeys = _.sortBy(getAllKeys);
-
-  const result = sortAllKeys.reduce((acc, key) => {
-    if (Object.hasOwn(parseFile1, key) && !Object.hasOwn(parseFile2, key)) {
-      acc.push([`${'- '}${key}`, parseFile1[key]]);
-      return acc;
-    }
-    if (!Object.hasOwn(parseFile1, key) && Object.hasOwn(parseFile2, key)) {
-      acc.push([`${'+ '}${key}`, parseFile2[key]]);
-      return acc;
-    }
-    if (Object.hasOwn(parseFile1, key) && Object.hasOwn(parseFile2, key)) {
-      if (parseFile1[key] === parseFile2[key]) {
-        acc.push([`${'  '}${key}`, parseFile1[key]]);
-        return acc;
-      }
-      acc.push([`${'- '}${key}`, parseFile1[key]]);
-      acc.push([`${'+ '}${key}`, parseFile2[key]]);
-      return acc;
-    }
-    return acc;
-  }, []);
-
-  return JSON.stringify(Object.fromEntries(result), null, 2);
+  return makeTree(parseFile1, parseFile2);
 };
 
 // console.log(genDiff('./src/file1.json', './src/file2.json'));
