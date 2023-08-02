925f96678515cac64b454affb505e2682aafecd1
Nikolai Khalimendik <kolyakhalimendik@gmail.com>
2023-08-02 15:42:16 +0300

    modified makeTree.js

--- src/makeTree.js
+++ src/makeTree.js
@@ -6,28 +6,28 @@ const makeTree = (file1, file2) => {
   const getAllKeys = _.union(getKeysFile1, getKeysFile2);
   const sortAllKeys = _.sortBy(getAllKeys);
 
-  const result = sortAllKeys.reduce((acc, key) => {
+  const arrKeyValue = sortAllKeys.reduce((acc, key) => {
     if (Object.hasOwn(file1, key) && !Object.hasOwn(file2, key)) {
-      acc.push([`${'- '}${key}`, file1[key]]);
+      acc.push([`- ${key}`, file1[key]]);
       return acc;
     }
     if (!Object.hasOwn(file1, key) && Object.hasOwn(file2, key)) {
-      acc.push([`${'+ '}${key}`, file2[key]]);
+      acc.push([`+ ${key}`, file2[key]]);
       return acc;
     }
     if (Object.hasOwn(file1, key) && Object.hasOwn(file2, key)) {
       if (file1[key] === file2[key]) {
-        acc.push([`${'  '}${key}`, file1[key]]);
+        acc.push([`  ${key}`, file1[key]]);
         return acc;
       }
-      acc.push([`${'- '}${key}`, file1[key]]);
-      acc.push([`${'+ '}${key}`, file2[key]]);
+      acc.push([`- ${key}`, file1[key]]);
+      acc.push([`+ ${key}`, file2[key]]);
       return acc;
     }
     return acc;
   }, []);
-
-  return JSON.stringify(Object.fromEntries(result), null, 2);
+  const result = JSON.stringify(Object.fromEntries(arrKeyValue), null, 2);
+  return result.replace(/"([^"]+)":/g, '$1:').replace(/"([^"]+)",/g, '$1,');
 };
 
 export default makeTree;
